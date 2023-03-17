7644a11f640f31d5ae169939d0d83167aa3eabde
Kasper Wolsink <kwolsink@gmail.com>
2023-03-17 15:20:46 +0100

    Install bulma

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,7 @@
 <template>
-  Hello world!
+  <div class="container">
+    <h1 class="title">Hello, world!</h1>
+    <p class="subtitle">This is a Nuxt 3 project with Bulma CSS.</p>
+    <button class="button is-ghost">hello world</button>
+  </div>
 </template>
