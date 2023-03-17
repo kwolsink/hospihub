570cab03c3d13a46e4897203944a2bbb3f24684b
Kasper Wolsink <kwolsink@gmail.com>
2023-03-18 00:42:58 +0100

    implement use firebase

--- composables/useFirebase.ts
+++ composables/useFirebase.ts
@@ -1,15 +1,34 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth"
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from "firebase/auth"
 
 
 export const useFireBase = () => {
   const provider = new GoogleAuthProvider();
   const auth = getAuth();
   const signInWithGoogle = () => signInWithPopup(auth, provider).then((result) => {
-    console.log("logged in!")
   })
 
-  return { signInWithGoogle }
+
+  const uid = ref();
+  const username = ref();
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      uid.value = user?.uid;
+      username.value = user?.displayName;
+    } else {
+      uid.value = null;
+      username.value = null;
+    }
+  });
+
+  const isAuthenticated = () => {return uid.value != null;}
+
+
+
+  return { signInWithGoogle, isAuthenticated}
 }
+
+
 
 
 
