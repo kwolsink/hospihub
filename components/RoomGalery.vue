f399ca6a94c47d498270f55e264a15b0bd28d31d
Kasper Wolsink <kwolsink@gmail.com>
2023-03-17 16:02:10 +0100

    create seperate room galery component

---
+++ components/RoomGalery.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="columns">
+    <div v-for="room in rooms" class="column is-one-third">
+        <RoomCard :room="room">
+        </RoomCard>
+      </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const rooms = [
+  {
+    thumbnail: "room.jpeg",
+    description: "foo bar",
+    title: "Gezellige kamer in het centrum van Amsterdam."
+  },
+  {
+    thumbnail: "room.jpeg",
+    description: "foo bar",
+    title: "Ongezellige kamer in  centrum van Almere."
+  },
+  {
+    thumbnail: "room.jpeg",
+    description: "foo bar",
+    title: "foo"
+  }
+]
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,13 @@
 <template>
   <div>
-    <div class="columns">
-      <div v-for="room in rooms" class="column is-one-third">
-        <RoomCard :room="room">
-        </RoomCard>
-      </div>
-    </div>
-
+    <RoomGalery/>
   </div>
 </template>
 
 
 <script setup lang="ts">
 
-const rooms = [
-  {
-    thumbnail: "room.jpeg",
-    description: "foo bar",
-    title: "Gezellige kamer in het centrum van Amsterdam."
-  },
-  {
-    thumbnail: "room.jpeg",
-    description: "foo bar",
-    title: "Ongezellige kamer in  centrum van Almere."
-  },
-  {
-    thumbnail: "room.jpeg",
-    description: "foo bar",
-    title: "foo"
-  }
-]
+
 
 
 </script>
