5513776d2400e68e796763fe867625a3c70f837d
Kasper Wolsink <kwolsink@gmail.com>
2023-03-17 18:18:51 +0100

    add roomfull

---
+++ components/RoomFull.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="columns is-centered">
+    <div class="column is-four-fifths">
+      <section class="section title">
+        <h1>{{ room?.title }}</h1>
+      </section>
+      <section class="section description">
+        <h3>Beschrijving</h3>
+        <p>{{ room?.description }}</p>
+      </section>
+
+      <div class="carousel">
+        <div>
+          elem 1
+        </div>
+        <div>
+          elem 1
+        </div>
+        <div>
+          elem 1
+        </div>
+        <div>
+          elem 1
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+
+
+defineProps({
+  room: Object
+})
+
+
+</script>
--- pages/rooms/[id].vue
+++ pages/rooms/[id].vue
@@ -1,3 +1,16 @@
 <template>
-  room id
+  <div>
+    <RoomFull :room="room"></RoomFull>
+  </div>
 </template>
+
+<script setup lang="ts">
+const route = useRoute()
+
+const room = {
+  title: "Leuke kamer in roffa",
+  description: "lorem fjpewf aope fjja pfjoa ejopfjaew ofejp afef"
+}
+
+
+</script>
