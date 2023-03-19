2c203b36c9675925d94786fbc08389f4a8e1359f
Kasper Wolsink <kwolsink@gmail.com>
2023-03-19 15:56:37 +0100

    add chat sample data

---
+++ samples/chats.ts
@@ -0,0 +1,14 @@
+import Chat from "~~/shared/types/Chat";
+
+const chat1: Chat = {
+  id: "1",
+  contact: "Pieter",
+  lastMessage: "Dat klinkt goed, bij deze nodigen we je uit op de instemming op de jacoba van beierenlaan 1"
+}
+
+const chat2: Chat = {
+  id: "2",
+  contact: "Freek"
+}
+
+export default [chat1, chat2]
