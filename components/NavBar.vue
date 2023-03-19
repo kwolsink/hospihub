c1142b23d902ef8185ec83decdc519cdc67ec307
Kasper Wolsink <kwolsink@gmail.com>
2023-03-19 12:34:10 +0100

    add profile section to navbar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -26,27 +26,10 @@
           Plaats een kamer
         </NuxtLink>
 
-        <div class="navbar-item has-dropdown is-hoverable">
-          <a class="navbar-link">
-            More
-          </a>
+        <NuxtLink v-if="firebase?.currentUser" to="/" class="navbar-item has-text-info">
+          Profiel
+        </NuxtLink>
 
-          <div class="navbar-dropdown">
-            <a class="navbar-item">
-              About
-            </a>
-            <a class="navbar-item">
-              Jobs
-            </a>
-            <a class="navbar-item">
-              Contact
-            </a>
-            <hr class="navbar-divider">
-            <a class="navbar-item">
-              Report an issue
-            </a>
-          </div>
-        </div>
       </div>
 
       <div class="navbar-end">
