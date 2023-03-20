917c2efa6e701c1d55bb94607ce65d79fb97802c
Kasper Wolsink <kwolsink@gmail.com>
2023-03-20 19:13:17 +0100

    fix room full class

--- components/RoomFull.vue
+++ components/RoomFull.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="room-full">
     <section class="section title">
-      <h1>{{ room?.title }}</h1>
+      <h1>{{ room.metadata.data.title }}</h1>
     </section>
 
     <section class="section images">
       <div class="columns">
-        <figure v-for="image in room?.images" class="column image room-image">
+        <figure v-for="image in room.metadata.data.images" class="column image room-image">
           <img :src="`../${image}`">
         </figure>
       </div>
@@ -14,11 +14,11 @@
     <div class="is-flex is-flex-direction-column">
       <div class="tags">
         <span class="tag mr-1">{{ rentText }}</span>
-        <span v-if="room.amountOfRoommates != null" class="tag mr-1">{{ roomMatesText }}</span>
+        <span v-if="room.metadata.data.description != null" class="tag mr-1">{{ roomMatesText }}</span>
       </div>
       <div class="description">
         <h3>Beschrijving</h3>
-        <p>{{ room?.description }}</p>
+        <p>{{ room.metadata.data.description }}</p>
       </div>
       <section class="reaction-section mt-5">
         <button v-if="!firebase?.currentUser" class="button is-primary m-5" @click="firebase?.signInWithGoogle()">
@@ -33,7 +33,7 @@
 </template>
 
 <script setup lang="ts">
-import Room from '~~/shared/types/Room';
+import {Room} from '~~/shared/types/Room';
 
 const firebase = ref()
 
@@ -49,18 +49,18 @@ const props = defineProps({
   }
 });
 const rentText = computed(() => {
-  if (props.room.rent != null) {
-    return `€${props.room.rent} p/m`
+  if (props.room.metadata.data.rent != null) {
+    return `€${props.room.metadata.data.rent} p/m`
   }
   return "Huur onbekend"
 });
 
 
 const roomMatesText = computed(() => {
-  if (props.room.amountOfRoommates === 1) {
-    return `${props.room.amountOfRoommates} huisgenoot`;
+  if (props.room.metadata.data.amountOfRoommates === 1) {
+    return `${props.room.metadata.data.amountOfRoommates} huisgenoot`;
   }
-  return `${props.room.amountOfRoommates} huisgenoten`;
+  return `${props.room.metadata.data.amountOfRoommates} huisgenoten`;
 });
 
 </script>
